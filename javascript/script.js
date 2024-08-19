854e51188aaf75afd1dceacd1b4bdeb40d8b7407
flashboostsite <lhomes455@gmail.com>
2024-08-19 00:05:15 -0400

    Create script.js

---
+++ javascript/script.js
@@ -0,0 +1,16 @@
+    var acc = document.getElementsByClassName("accordion");
+    for (var i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var panel = this.nextElementSibling;
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+            }
+        });
+    }
+
+    function toggleMenu() {
+        document.querySelector('.nav-links').classList.toggle('active');
+    }
